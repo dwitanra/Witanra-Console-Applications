ed43b87beb27c9a6cb209361893243e392bb16a3
Denis Witanra <denis@witanra.com>
2020-07-05 13:01:58 -0500

    Adds DeepStack AI

---
+++ Witanra.DeepStack/Models/DeepStackResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Witanra.DeepStack.Models
+{
+    internal class DeepStackResponse
+    {
+        public bool success { get; set; }
+        public DeepStackPredection[] predictions { get; set; }
+    }
+}
---
+++ Witanra.DeepStack/Program.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Witanra.DeepStack.Models;
+
+namespace Witanra.DeepStack
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var files = Directory.GetFiles("Samples");
+            foreach (var file in files)
+            {
+                detectFace(file).Wait();
+            }
+        }
+
+        private static HttpClient client = new HttpClient();
+
+        public static async Task detectFace(string filename)
+        {
+            var request = new MultipartFormDataContent();
+            var image_data = File.OpenRead(filename);
+            request.Add(new StreamContent(image_data), "image", Path.GetFileName(filename));
+            var output = await client.PostAsync("http://localhost:8080/v1/vision/detection", request);
+            var jsonString = await output.Content.ReadAsStringAsync();
+            var response = JsonConvert.DeserializeObject<DeepStackResponse>(jsonString);
+
+            var saved_Filename = "Output" + filename;
+            Save_Image_with_Result(filename, response, saved_Filename);
+
+            foreach (var user in response.predictions)
+            {
+                Console.WriteLine(user.label);
+            }
+
+            Console.WriteLine(jsonString);
+        }
+
+        private static void Save_Image_with_Result(string orginal_Filename, DeepStackResponse response, string saved_Filename)
+        {
+            Bitmap bitmap = (Bitmap)Image.FromFile(orginal_Filename);
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                foreach (var predection in response.predictions)
+                {
+                    Pen pen = new Pen(Brushes.Red);
+
+                    Rectangle rect = new Rectangle(predection.x_min, predection.y_min, predection.x_max - predection.x_min, predection.y_max - predection.y_min);
+                    graphics.DrawRectangle(pen, rect);
+
+                    graphics.DrawString(predection.label, new Font("Arial", 16), Brushes.Red, new Point(predection.x_min, predection.y_min));
+                }
+            }
+
+            Directory.CreateDirectory(Path.GetDirectoryName(saved_Filename));
+            bitmap.Save(saved_Filename, ImageFormat.Jpeg);
+        }
+    }
+}
